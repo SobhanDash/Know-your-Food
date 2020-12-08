2b84fadca448843965ee04968e2d5e37bae03096
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2020-12-08 16:32:41 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+const meaningStyle = {
+  fontSize: "2rem",
+  color: "blueviolet",
+  padding: "1rem",
+  display: "block",
+  margin: "0.5rem auto",
+  width: "70%",
+  border: "none",
+  borderRadius: "0.5rem",
+  backgroundColor: "#212322"
+};
+
+const inputstyle = {
+  display: "block",
+  margin: "auto",
+  padding: "1rem",
+  width: "70%",
+  backgroundColor: "#212322",
+  border: "none",
+  borderRadius: "0.5rem",
+  color: "white",
+  fontSize: "1.2rem"
+};
+
+const choice = {
+  fontSize: "2rem",
+  color: "green",
+  padding: "0.5rem"
+};
+
+const cursor = {
+  fontSize: "2.5rem",
+  padding: "0.5rem",
+  cursor: "pointer"
+};
+
 const fooddict = {
-  "🍈": "Melon",
-  "🥑": "Avocado",
-  "🍐": "Pear",
-  "🍊": "Tangerine",
-  "🥝": "Kiwi",
-  "🥨": "Pretzel",
-  "🍡": "Dango",
+  "🍔": "Hamburger",
+  "🍟": "French Fries",
+  "🍕": "Pizza",
+  "🌭": "Hot Dog",
+  "🥪": "Sandwich",
+  "🌮": "Taco",
+  "🌯": "Burrito",
+  "🥙": "Stuffed Flatbread",
   "🧆": "Falafel",
-  "🍙": "Rice ball",
-  "🍢": "Oden"
+  "🍱": "Bento Box",
+  "🍘": "Rice Cracker",
+  "🍙": "Rice Ball",
+  "🍚": "Cooked Rice",
+  "🍛": "Curry Rice",
+  "🍜": "Steaming Bowl",
+  "🍝": "Spaghetti",
+  "🍠": "Roasted Sweet Potato",
+  "🍢": "Oden",
+  "🍣": "Sushi",
+  "🍤": "Fried Shrimp",
+  "🍥": "Fish Cake with Swirl",
+  "🥮": "Moon Cake",
+  "🍡": "Dango",
+  "🥟": "Dumpling",
+  "🥠": "Fortune Cookie",
+  "🥡": "Takeout Box",
+  "🦪": "Oyster",
+  "🍇": "Grapes",
+  "🍈": "Melon",
+  "🍉": "Watermelon",
+  "🍊": "Tangerine",
+  "🍋": "Lemon",
+  "🍌": "Banana",
+  "🍍": "Pineapple",
+  "🥭": "Mango",
+  "🍎": "Red Apple",
+  "🍏": "Green Apple",
+  "🍐": "Pear",
+  "🍑": "Peach",
+  "🍒": "Cherries",
+  "🍓": "Strawberry",
+  "🥨": "Pretzel",
+  "🥯": "Bagel",
+  "🥞": "Pancakes",
+  "🧇": "Waffle",
+  "🧀": "Cheese Wedge",
+  "🍖": "Meat on Bone",
+  "🍗": "Poultry Leg",
+  "🥩": "Cut of Meat",
+  "🥓": "Bacon",
+  "🍦": "Soft Ice Cream",
+  "🍧": "Shaved Ice",
+  "🍨": "Ice Cream",
+  "🍩": "Doughnut",
+  "🍪": "Cookie",
+  "🎂": "Birthday Cake",
+  "🍰": "Shortcake",
+  "🧁": "Cupcake",
+  "🥧": "Pie",
+  "🍫": "Chocolate Bar",
+  "🍬": "Candy",
+  "🍭": "Lollipop",
+  "🍮": "Custard"
 };
 
 var foodweknow = Object.keys(fooddict);
 
 export default function App() {
   const [meaning, setmeaning] = useState("");
+  const [emojiindex, setemojiindex] = useState(-1);
 
   function foodInputHandler(event) {
     var userInput = event.target.value;
@@ -26,6 +116,7 @@ export default function App() {
 
     if (meaning === undefined) {
       meaning = "We don't have it in our database";
+      setemojiindex(-1);
     }
     setmeaning(meaning);
   }
@@ -37,26 +128,31 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1 style={{ fontSize: "3rem", color: "red" }}>Know your Food!</h1>
+      <h1>Know your Food!</h1>
+      <h2> Select or Enter a food/drink emoji</h2>
+
       <input
         onChange={foodInputHandler}
-        style={{ height: "2rem", width: "60%", border: "1.5px solid black" }}
+        placeholder="Enter your emoji"
+        style={inputstyle}
+        type="text"
       />
 
-      <div style={{ fontSize: "2rem", color: "blueviolet", padding: "0.5rem" }}>
-        {" "}
-        {meaning}{" "}
-      </div>
+      {emojiindex === -1 ? (
+        <h2 style={meaningStyle}> Meaning: {meaning} </h2>
+      ) : (
+        <h2 style={meaningStyle}>
+          {choice} : {meaning}
+        </h2>
+      )}
 
-      <div style={{ fontSize: "2rem", color: "green", padding: "0.5rem" }}>
-        {" "}
-        What we know{" "}
-      </div>
+      <div style={choice}> Click on your choice! </div>
+
       {foodweknow.map(function (emoji) {
         return (
           <span
             onClick={() => foodClickHandler(emoji)}
-            style={{ fontSize: "2.5rem", padding: "0.5rem", cursor: "pointer" }}
+            style={cursor}
             key={emoji}
           >
             {emoji}
